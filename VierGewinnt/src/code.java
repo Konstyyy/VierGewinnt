e219b593f65e4c2e2f7e391162f03060e3d0a4b0
Konstantin H. <konstantinhellmann6@gmail.com>
2022-01-14 09:19:57 +0100

    fb

---
+++ VierGewinnt/src/code.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class code {
+
+	public static void main(String[] args) {
+		
+		JFrame window = new JFrame();
+		window.setSize(600, 400);
+		window.setVisible(true);
+		window.setTitle("Vier Gewinnt");
+		
+	}
+
+}
